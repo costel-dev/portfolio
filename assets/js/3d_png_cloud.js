a7f317d9355b478a905c91ff09ebf6a1420c0a59
costel-dev <costelcuconoiu@gmail.com>
2020-12-17 10:25:35 +0100

    full path for photo skills added

--- assets/js/3d_png_cloud.js
+++ assets/js/3d_png_cloud.js
@@ -2,112 +2,112 @@
 $(document).ready(function() {
 var entries = [
     {
-        image: "../img/vectorPL/code.png",
+        image: "../../assets/img/vectorPL/code.png",
         width: "50",
         height: "50",
         url: "https://aws.amazon.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/bootstrap.png",
+        image: "../../assets/img/vectorPL/bootstrap.png",
         width: "50",
         height: "50",
         url: "https://getbootstrap.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/css3.png",
+        image: "../../assets/img/vectorPL/css3.png",
         width: "50",
         height: "50",
         url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/firebase.png",
+        image: "../../assets/img/vectorPL/firebase.png",
         width: "50",
         height: "50",
         url: "https://firebase.google.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/git.png",
+        image: "../../assets/img/vectorPL/git.png",
         width: "50",
         height: "50",
         url: "https://git-scm.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/html5.png",
+        image: "../../assets/img/vectorPL/html5.png",
         width: "50",
         height: "50",
         url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/javascript.png",
+        image: "../../assets/img/vectorPL/javascript.png",
         width: "50",
         height: "50",
         url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/mongodb.png",
+        image: "../../assets/img/vectorPL/mongodb.png",
         width: "50",
         height: "50",
         url: "https://www.mongodb.com/1",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/mysql.png",
+        image: "../../assets/img/vectorPL/mysql.png",
         width: "50",
         height: "50",
         url: "https://www.mysql.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/nodejs.png",
+        image: "../../assets/img/vectorPL/nodejs.png",
         width: "50",
         height: "50",
         url: "https://nodejs.org/en/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/reactjs.png",
+        image: "../../assets/img/vectorPL/reactjs.png",
         width: "50",
         height: "50",
         url: "https://reactjs.org/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/vscode.png",
+        image: "../../assets/img/vectorPL/vscode.png",
         width: "50",
         height: "50",
         url: "https://code.visualstudio.com/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/vue.png",
+        image: "../../assets/img/vectorPL/vue.png",
         width: "50",
         height: "50",
         url: "https://vuejs.org/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/redux.png",
+        image: "../../assets/img/vectorPL/redux.png",
         width: "50",
         height: "50",
         url: "https://redux.js.org/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/slack.png",
+        image: "../../assets/img/vectorPL/slack.png",
         width: "50",
         height: "50",
         url: "https://slack.com/intl/en-de/",
         target: "_blank"
     },
     {
-        image: "../img/vectorPL/github.png",
+        image: "../../assets/img/vectorPL/github.png",
         width: "50",
         height: "50",
         url: "https://github.com/",
